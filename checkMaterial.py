ed1820e6595ce147dc938bfcb52264c7113d93f6
aiyl <38548129+aiyl@users.noreply.github.com>
2020-05-01 01:25:54 +1000

    Start Check material
    try to find minimal difference between two diffuse color and check (just for the future)

--- checkMaterial.py
+++ checkMaterial.py
@@ -5,8 +5,14 @@ def getDiffuseColor (path):
     while bool:
         if 'Kd' in line:
             bool = False
-            print(line)
+            a = [a for a in line.split(' ') ]
         line = f.readline()
+    a.remove(a[0])
     f.close()
+    return a
+diff = []
+for i in range(len(getDiffuseColor("check_mtl_blend.mtl"))):
+    diff.append(float(getDiffuseColor("check_mtl_blend.mtl")[i]) - float(getDiffuseColor('solve2.mtl')[i]))
+print(diff)
+print(getDiffuseColor("check_mtl_blend.mtl"), getDiffuseColor('solve2.mtl'))
 
-getDiffuseColor("solve2.mtl")
