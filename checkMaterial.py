c5635bcccd155e9de20d214620578ebcfaa8103b
aiyl <38548129+aiyl@users.noreply.github.com>
2020-05-03 01:26:55 +1000

    FAilllll
    Try to find minimal difference between two color, but ddn't know about formula. Will try again tommorow

--- checkMaterial.py
+++ checkMaterial.py
@@ -1,18 +1,42 @@
 def getDiffuseColor (path):
     f = open(path)
     line = f.readline()
-    bool = True
-    while bool:
+    a = []
+    while True:
         if 'Kd' in line:
-            bool = False
-            a = [a for a in line.split(' ') ]
+            #a = [a for a in line.split(' ') ]
+            a.append(line.split(' '))
         line = f.readline()
-    a.remove(a[0])
+        if not line:
+            break
+    for i in range (len(a)):
+        a[i].remove(a[i][0])
     f.close()
     return a
-diff = []
-for i in range(len(getDiffuseColor("check_mtl_blend.mtl"))):
-    diff.append(float(getDiffuseColor("check_mtl_blend.mtl")[i]) - float(getDiffuseColor('solve2.mtl')[i]))
-print(diff)
-print(getDiffuseColor("check_mtl_blend.mtl"), getDiffuseColor('solve2.mtl'))
+def findMinDiff (list1, list2) :
 
+    for i in range(len(list2)):
+        min = [1.0, 1.0, 1.0]
+        sumMin = 3
+        for m in range(len(list1)):
+            sum = 0
+            for k in  range(len(list2[0])):
+                sum += abs(float(list1[m][k]) - float(list2[i][k]))
+            if sumMin > sum:
+                min=list1[m]
+                sumMin = sum
+
+               # if min[k] > abs(float(list1[m][k]) - float(list2[i][k])):
+                    #min[k] = abs(float(list1[m][k]) - float(list2[i][k]))
+                #min.append(abs(float(list1[i][k]) - float(list2[i][k])))
+
+        print(min )
+
+
+diff = []
+#for i in range(len(getDiffuseColor("check_mtl_blend.mtl"))):
+#    diff.append(float(getDiffuseColor("check_mtl_blend.mtl")[i]) - float(getDiffuseColor('solve2.mtl')[i]))
+#print(diff)
+findMinDiff(getDiffuseColor('isbaForBlend.mtl'), getDiffuseColor('Check_Material.mtl'))
+print('участник ',getDiffuseColor('isbaForBlend.mtl'), '\n', 'эталон ',  getDiffuseColor('Check_Material.mtl'))
+#a[0.1.2]=[h=0.5, s= ,v=0.05]
