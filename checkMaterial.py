ac3bb52ff7341c10c1e05a9cd6fd455552447da4
aiyl <38548129+aiyl@users.noreply.github.com>
2020-05-04 00:39:42 +1000

    convert rgb to lab. Find minimal between materials delta E - CIE 76
    в след раз нужно систему оценивания вести
    CIE 94 - не подходит

--- checkMaterial.py
+++ checkMaterial.py
@@ -1,3 +1,6 @@
+import skimage
+from skimage import io, color
+
 def getDiffuseColor (path):
     f = open(path)
     line = f.readline()
@@ -14,29 +17,28 @@ def getDiffuseColor (path):
     f.close()
     return a
 def findMinDiff (list1, list2) :
-
+    min = 300.0
     for i in range(len(list2)):
-        min = [1.0, 1.0, 1.0]
-        sumMin = 3
         for m in range(len(list1)):
-            sum = 0
-            for k in  range(len(list2[0])):
-                sum += abs(float(list1[m][k]) - float(list2[i][k]))
-            if sumMin > sum:
-                min=list1[m]
-                sumMin = sum
+            for k in range(len(list2[0])):
+                list1[m][k] = float(list1[m][k])
+                list2[i][k] = float(list2[i][k])
+            #print('List1 ', list1[m])
+            lab1 = color.rgb2lab([[list1[m]]],'D65','2')
+            lab2 = color.rgb2lab([[list2[i]]],'D65','2')
+            #print(lab1)
+            #print('List2 ',list2[i])
+            #print(lab2)
+            #print('lol ',skimage.color.deltaE_ciede94(lab1, lab2, 2, 0.048, 0.014))
+            if min > skimage.color.deltaE_cie76(lab1, lab2):
+                min = skimage.color.deltaE_cie76(lab1, lab2)
+                min1 = list1[m]
+                min2 = list2[i]
+            #print('Delta ',skimage.color.deltaE_cie76(lab1, lab2))
+        print('List1 ', min1)
+        print('List2 ', min2)
+        print(min)
+findMinDiff(getDiffuseColor('check2.mtl'), getDiffuseColor('check1.mtl'))
+print('участник ',getDiffuseColor('check2.mtl'), '\n', 'эталон ',  getDiffuseColor('check1.mtl'))
 
-               # if min[k] > abs(float(list1[m][k]) - float(list2[i][k])):
-                    #min[k] = abs(float(list1[m][k]) - float(list2[i][k]))
-                #min.append(abs(float(list1[i][k]) - float(list2[i][k])))
-
-        print(min )
-
-
-diff = []
-#for i in range(len(getDiffuseColor("check_mtl_blend.mtl"))):
-#    diff.append(float(getDiffuseColor("check_mtl_blend.mtl")[i]) - float(getDiffuseColor('solve2.mtl')[i]))
-#print(diff)
-findMinDiff(getDiffuseColor('isbaForBlend.mtl'), getDiffuseColor('Check_Material.mtl'))
-print('участник ',getDiffuseColor('isbaForBlend.mtl'), '\n', 'эталон ',  getDiffuseColor('Check_Material.mtl'))
 #a[0.1.2]=[h=0.5, s= ,v=0.05]
